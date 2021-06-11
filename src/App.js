54abfb2c0be06359c013cc5d55aa633baf38e1eb
Emine Kebeli <kbl.emine@gmail.com>
2021-06-11 15:43:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import pages from "./data/pages";
 import { Context } from "./context/LanguageContext";
 import { GlobalProvider } from "./context/GlobalState";
+import ReactGA from "react-ga"
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-EYXW0CL7FR")
+    ReactGA.pageview(window.location.pathname + window.location.search)
+    console.log(window.location.pathname + window.location.search)
+
+  }, [window.location.pathname])
   const routeMaps = pages.map((item, index) => (
     <Route
       key={index}
