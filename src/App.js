dcb4a04df26265b5a940eb4bf38473606f11bcaf
Emine Kebeli <kbl.emine@gmail.com>
2021-06-11 15:47:08 +0300

    react ga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import pages from "./data/pages";
 import { Context } from "./context/LanguageContext";
